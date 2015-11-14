bf33dbdcabd4b4f5bb0185cfbd0730e81c8e6bcb
student <student@usc-secure-wireless-064-019.usc.edu>
2015-11-14 13:36:34 -0800

    Initial commit

--- Grub Buddies/PeopleListView.swift
+++ Grub Buddies/PeopleListView.swift
@@ -1,17 +1,14 @@
 //
-//  ViewController.swift
+//  File.swift
 //  Grub Buddies
 //
 //  Created by student on 11/14/15.
 //  Copyright © 2015 2366Portlanders. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class PeopleListView : UITableViewController{
     
-
-
 }
-
