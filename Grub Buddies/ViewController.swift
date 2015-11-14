02aa59be96023e11e914f9609838b50ddbea1b71
student <student@eccloaner-03m.local>
2015-11-14 11:13:59 -0800

    Initial Commit

---
+++ Grub Buddies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Grub Buddies
+//
+//  Created by student on 11/14/15.
+//  Copyright © 2015 2366Portlanders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
