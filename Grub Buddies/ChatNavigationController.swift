f17b98873cf0d309876e59a96e8b22a8f9e8dd08
student <student@usc-secure-wireless-064-019.usc.edu>
2015-11-14 15:45:39 -0800

    Commit no 2

---
+++ Grub Buddies/ChatNavigationController.swift
@@ -0,0 +1,15 @@
+//
+//  ChatNavigationController.swift
+//  Grub Buddies
+//
+//  Created by student on 11/14/15.
+//  Copyright © 2015 2366Portlanders. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ChatNavigationController : UINavigationController{
+    override func presentViewController(viewControllerToPresent: UIViewController, animated flag: Bool, completion: (() -> Void)?) {
+    }
+}
--- Grub Buddies/PeopleListView.swift
+++ Grub Buddies/PeopleListView.swift
@@ -9,6 +9,44 @@
 import Foundation
 import UIKit
 
-class PeopleListView : UITableViewController{
+class PeopleListView: UITableViewController{
+    var currentPerson : String?
     
+    override func unwindForSegue(unwindSegue: UIStoryboardSegue, towardsViewController subsequentVC: UIViewController) {
+        if unwindSegue.identifier == "checkInSegue"{
+            print("Moved to People List")
+        }
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let tableCell = UITableViewCell()
+        tableCell.textLabel!.text = "First Person"
+        let image : UIImage = (UIImage(named: "bg"))!
+        tableCell.imageView!.image = image
+        return tableCell
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        currentPerson = "First Person"
+        let ac = UIAlertController(title: "Introduce yourself!", message: "Leave a message for your Grub Buddy", preferredStyle: .Alert)
+        ac.addTextFieldWithConfigurationHandler(nil)
+        
+        let introduceAction = UIAlertAction(title: "Introduce", style: .Default) { [unowned self, ac] (action: UIAlertAction!) in
+            let intro = ac.textFields![0] as! UITextField
+                print(intro.text)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Default) { [unowned self, ac] (action: UIAlertAction!) in
+            
+        }
+        
+        ac.addAction(introduceAction)
+        ac.addAction(cancelAction)
+        
+        presentViewController(ac, animated: true, completion: nil)
+    }
 }
